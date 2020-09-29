d0a01972f93b7099c39c93f1dfcb30b94a1b6793
Vicky Liu <apple87511@gmail.com>
2020-09-29 21:18:13 +0800

    refactor

---
+++ products-2.py
@@ -0,0 +1,47 @@
+import os
+
+#讀取檔案
+def read_file(filename):
+	products=[]
+	with open(filename, 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue #繼續
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	return products
+
+
+#讓使用者輸入
+def user_input(products):
+	products = []
+	while True:
+		name = input('請輸入商品名稱: ')
+		if name == 'q':
+			break
+		price = input(' 請輸入商品價格: ')
+		products.append([name, price])
+	return products
+
+#印出所有購買紀錄
+def print_products(products):
+	for p in products:
+		print(p[0], '的價格是', p[1])
+ 
+#寫入檔案
+def write_file(filename, products):
+	with open(filename, 'w', encoding='utf-8') as f:
+		f.write('商品,價格\n')
+		for p in products:
+			f.write(p[0] + ',' + p[1] + '\n')
+
+def main():
+	if os.path.isfile('products.csv'): #檢查檔案在不在
+		print('yeah!找到檔案')
+		products = read_file('products.csv')
+	else:
+		print('找不到檔案')
+	products = user_input(products)
+	print_products(products)
+	write_file('products.csv', products)
+main()
